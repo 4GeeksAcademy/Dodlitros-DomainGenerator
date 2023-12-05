b5aec25ed54f9746c72fa160a243a769c032057d
Carlos beltran <137569880+DodLitros@users.noreply.github.com>
2023-12-05 00:41:50 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,47 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = [
+  "el",
+  "nuestro",
+  "tu",
+  "mi",
+  "su",
+  "ese",
+  "cualquier",
+  "algun",
+  "lo",
+  "donde"
+];
+let adj = [
+  "grande",
+  "pequeno",
+  "rápido",
+  "brillante",
+  "innovador",
+  "divertido",
+  "original",
+  "creativo",
+  "interesante",
+  "wiki",
+  "banda"
+];
+let noun = [
+  "restaurante",
+  "blog",
+  "proyecto",
+  "equipo",
+  "sitio",
+  "evento",
+  "viaje",
+  "negocio",
+  "libro",
+  "juego",
+  "programa"
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let i = 0; i < 11; i++) {
+  console.log(
+    pronoun[Math.floor(Math.random() * pronoun.length)] +
+      adj[Math.floor(Math.random() * adj.length)] +
+      noun[Math.floor(Math.random() * noun.length)] +
+      ".com"
+  );
+}
